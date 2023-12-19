5e81c3ea6b3667210db34df0469f0aa7ec4e5484
Guilherme Athayde <guiathayde303@gmail.com>
2023-12-19 14:26:54 -0300

    :sparkles: Add Splash Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppProvider } from './hooks';
@@ -6,6 +7,10 @@ import { AppProvider } from './hooks';
 import { Routes } from './routes';
 
 export function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <AppProvider>
