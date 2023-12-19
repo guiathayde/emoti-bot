8df0ae76abcac77f2e30a5316c7e8004c0f855e3
Guilherme Athayde <guiathayde303@gmail.com>
2023-12-19 15:00:58 -0300

    :art: Finish About screen

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Image, View } from 'react-native';
+import { FlatList, Image, ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, Card, Text } from 'react-native-paper';
 
@@ -10,41 +10,30 @@ interface Info {
   id: string;
   title: string;
   description: string;
-  image: string;
+  image: any;
 }
 
 const infos: Info[] = [
   {
     id: '1',
-    title: 'Ansiedade',
+    title: 'UFSCar',
     description:
-      'A ansiedade é uma reação normal do organismo e funciona como um aviso que nos alerta sobre o desconhecido ou qualquer tipo de perigo. Quando o sentimento é passageiro, não há problema algum, mas quando a ansiedade se torna constante, ela pode se transformar em uma doença.',
-    image:
-      'https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'A Universidade Federal de São Carlos (UFSCar), estabelecida em 1968, é reconhecida como uma das principais instituições de ensino superior do Brasil. Com um campus vibrante e diversificado, a UFSCar oferece uma variedade de cursos em diferentes áreas do conhecimento, promovendo um ambiente de aprendizado dinâmico e inclusivo. A universidade se destaca pela excelência em pesquisa, ensino e extensão, contribuindo significativamente para o desenvolvimento científico, tecnológico e social. Além de sua infraestrutura de ponta, a UFSCar é conhecida por seu compromisso com a sustentabilidade e a responsabilidade social, buscando constantemente maneiras de melhorar a vida na comunidade local e global.',
+    image: require('../../assets/logo_ufscar.png'),
   },
   {
     id: '2',
-    title: 'Depressão',
+    title: 'Curso de Interação Humano-Computador',
     description:
-      'A depressão é uma doença psiquiátrica crônica e recorrente que produz alteração do humor caracterizada por uma tristeza profunda, sem fim, associada a sentimentos de dor, amargura, desencanto, desesperança, baixa autoestima e culpa, assim como a distúrbios do sono e do apetite.',
-    image:
-      'https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'A disciplina de Interação Humano-Computador (IHC) na UFSCar é parte fundamental do currículo do Departamento de Computação. Este curso explora os princípios e práticas de design de interfaces de usuário, centrando-se na criação de sistemas computacionais que sejam tanto funcionais quanto intuitivos. Através de uma combinação de teoria e prática, os alunos aprendem a projetar, avaliar e implementar interfaces de usuário eficazes, considerando a experiência e a perspectiva do usuário final. O curso enfatiza a importância da empatia e do entendimento das necessidades humanas no processo de design, preparando os alunos para enfrentar desafios reais e inovar na área de tecnologia.',
+    image: require('../../assets/ihc.png'),
   },
   {
     id: '3',
-    title: 'Estresse',
+    title: 'Desenvolvimento de um Aplicativo para Saúde Emocional dos Alunos',
     description:
-      'O estresse é uma reação do organismo com componentes físicos e mentais, que ocorre quando surge a necessidade de uma adaptação muito rápida a um evento ou situação inesperada.',
-    image:
-      'https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '4',
-    title: 'Insônia',
-    description:
-      'A insônia é um distúrbio do sono que pode ser caracterizado pela dificuldade de adormecer ou de permanecer dormindo durante a noite, fazendo com que a pessoa não consiga ter um sono reparador e acorde cansada e com sono no dia seguinte.',
-    image:
-      'https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?q=80&w=3000&auto=format&fit=crop&ixlib=rb-',
+      'Em resposta aos crescentes desafios de saúde mental enfrentados pelos alunos, a UFSCar está explorando o desenvolvimento de um aplicativo destinado a apoiar a saúde emocional dos estudantes do Departamento de Computação. Este aplicativo visa oferecer recursos como dicas de gerenciamento de estresse, exercícios de mindfulness, e um espaço seguro para expressar e entender emoções. A iniciativa reflete o compromisso da universidade em apoiar o bem-estar de seus alunos, reconhecendo a importância da saúde mental na educação superior.',
+    image: require('../../assets/mental_health.png'),
   },
 ];
 
@@ -80,29 +69,39 @@ export function About() {
         </Button>
       </View>
 
-      <FlatList
-        data={infos}
-        renderItem={({ item }) => (
-          <Card mode="contained" style={{ width: 400, marginHorizontal: 8 }}>
-            <Card.Title style={{ maxWidth: '100%' }} title={item.title} />
-            <Card.Content style={{ maxWidth: '100%' }}>
-              <Text variant="bodyMedium">{item.description}</Text>
-            </Card.Content>
-            <Card.Cover
+      <ScrollView>
+        <FlatList
+          data={infos}
+          renderItem={({ item }) => (
+            <Card
+              mode="contained"
               style={{
-                width: 200,
-                height: 200,
-                alignSelf: 'center',
-                marginTop: 16,
+                width: 400,
+                marginHorizontal: 8,
+                paddingTop: 16,
+                paddingBottom: 24,
               }}
-              source={{ uri: item.image }}
-            />
-          </Card>
-        )}
-        keyExtractor={item => item.id}
-        horizontal
-        style={{ flex: 1 }}
-      />
+            >
+              <Card.Title title={item.title} titleNumberOfLines={2} />
+              <Card.Content>
+                <Text variant="bodyMedium">{item.description}</Text>
+              </Card.Content>
+              <Card.Cover
+                style={{
+                  width: 200,
+                  height: 200,
+                  alignSelf: 'center',
+                  marginTop: 16,
+                }}
+                source={item.image}
+              />
+            </Card>
+          )}
+          keyExtractor={item => item.id}
+          horizontal
+          style={{ flex: 1 }}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
